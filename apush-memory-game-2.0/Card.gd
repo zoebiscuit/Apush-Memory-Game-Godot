13221c35f41d3d35b6fb4d848c1570864b497b22
Zoe Bisset <lemonadecode@gmail.com>
2026-03-31 13:00:37 -0400

    First commit, making game board

---
+++ apush-memory-game-2.0/Card.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+class_name Card
+
+var suit
+var value
+var face
+var back
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+	
